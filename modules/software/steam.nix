43d8feffbd2970f5d3f5caa37e3218f49a8e70bd
Sandbox-Freddy <31123359+Sandbox-Freddy@users.noreply.github.com>
2024-09-21 14:40:06 +0200

    [All hosts] flake update, change steam.nix [private] update hardware-configuration.nix

--- modules/software/steam.nix
+++ modules/software/steam.nix
@@ -9,13 +9,14 @@
     enable = lib.mkEnableOption "steam";
   };
   config = lib.mkIf config.modules.software.steam.enable {
-    programs.steam.enable = true;
-    programs.steam.gamescopeSession.enable = true;
-
-    environment.systemPackages = with pkgs; [
-      mangohud
-    ];
-
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      package = pkgs.unstable.steam.override {
+        extraPkgs = pkgs: [pkgs.attr];
+      };
+      gamescopeSession.enable = true;
+    };
     programs.gamemode.enable = true;
   };
 }
