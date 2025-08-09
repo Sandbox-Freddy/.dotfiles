9cdb06f5912b4ecac910ee52f8a200fd839db283
Sandbox-Freddy <31123359+Sandbox-Freddy@users.noreply.github.com>
2025-08-09 21:03:31 +0200

    [All hosts] - update ollama config

--- modules/software/ollama.nix
+++ modules/software/ollama.nix
@@ -4,18 +4,28 @@
   lib,
   hostVariables,
   ...
-}: {
+}: let
+  ollama-latest = pkgs.ollama.overrideAttrs (oldAttrs: rec {
+    version = "0.11.4";
+    src = pkgs.fetchFromGitHub {
+      owner = "ollama";
+      repo = "ollama";
+      rev = "v${version}";
+      sha256 = "joIA/rH8j+SJH5EVMr6iqKLve6bkntPQM43KCN9JTZ8=";
+    };
+    vendorHash = "sha256-SlaDsu001TUW+t9WRp7LqxUSQSGDF1Lqu9M1bgILoX4=";
+  });
+in {
   options.modules.software.ollama = {
     enable = lib.mkEnableOption "ollama ";
   };
 
   config = lib.mkIf config.modules.software.ollama.enable {
-    environment.systemPackages = with pkgs; [
-      unstable.ollama
-    ];
+    environment.systemPackages = [ollama-latest];
 
     services.ollama = {
       enable = true;
+      package = ollama-latest;
       acceleration = "cuda";
       loadModels = ["llama3.2:3b" "gpt-oss:20b"];
     };
