8a2ffc4c25079657bcd199c5bb716c39881fdc60
Freddy <31123359+Sandbox-Freddy@users.noreply.github.com>
2026-06-26 18:42:52 +0200

    [All hosts] - add cachix
    * [All hosts] - add Nix setup workflows with Cachix integration and nix-settings configuration
    
    * [All hosts] - fix incorrect path in setup-nix-cachix action
    
    * [All hosts] - update flake.lock
    
    * [All hosts] - temporally fix
    
    * [All hosts] - temporally fix
    
    * [All hosts] - temporally fix

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,15 @@
   config,
   ...
 }: {
+  # --- Overlay für OpenBLAS (i686) ---
+  nixpkgs.overlays = [
+    (final: prev: {
+      openblas =
+        if prev.stdenv.hostPlatform.system == "i686-linux"
+        then prev.openblas.overrideAttrs (_: { doCheck = false; })
+        else prev.openblas;
+    })
+  ];
   imports = [
     ./home.nix
   ];
