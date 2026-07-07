82b84ba160e8d2b468f3f2dc2dbbf79d46e4e5e5
Sandbox-Freddy <31123359+Sandbox-Freddy@users.noreply.github.com>
2026-07-07 19:37:12 +0200

    Update aliases.nix

--- modules/console/aliases.nix
+++ modules/console/aliases.nix
@@ -3,6 +3,7 @@
     rebuild = "sudo nixos-rebuild switch --flake /home/${hostVariables.username}/.dotfiles#${hostVariables.host} --impure";
     update = "nix flake update --flake /home/${hostVariables.username}/.dotfiles";
     switchnix = "nh os switch -H ${hostVariables.host} /home/${hostVariables.username}/.dotfiles --impure";
+    cleanup = "sudo nix-collect-garbage --delete-older-than 3d";
     nixfmt = "alejandra ./";
   };
 }
