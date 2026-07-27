c32fe753c0a489f18597b99bd9263dd45d5d7fbd
Sandbox-Freddy <31123359+Sandbox-Freddy@users.noreply.github.com>
2026-07-27 14:55:27 +0200

    [All hosts] - add templates folder

---
+++ templates/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development Environment Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+
+        ];
+        shellHook = ''
+          echo "Dev-Flake-Example"
+        '';
+      };
+    });
+}
